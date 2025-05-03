605eb9c12bb57b8e548845c071877f927287fbcf
XiaoyeAsZ <754857942@qq.com>
2025-05-03 20:56:11 +0800

    lab1.

---
+++ src/build/pony/CMakeFiles/pony.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/root/src/pony/mlir/Dialect.cpp" "pony/CMakeFiles/pony.dir/mlir/Dialect.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/Dialect.cpp.o.d"
+  "/root/src/pony/mlir/LowerToAffineLoops.cpp" "pony/CMakeFiles/pony.dir/mlir/LowerToAffineLoops.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/LowerToAffineLoops.cpp.o.d"
+  "/root/src/pony/mlir/LowerToLLVM.cpp" "pony/CMakeFiles/pony.dir/mlir/LowerToLLVM.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/LowerToLLVM.cpp.o.d"
+  "/root/src/pony/mlir/MLIRGen.cpp" "pony/CMakeFiles/pony.dir/mlir/MLIRGen.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/MLIRGen.cpp.o.d"
+  "/root/src/pony/mlir/PonyCombine.cpp" "pony/CMakeFiles/pony.dir/mlir/PonyCombine.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/PonyCombine.cpp.o.d"
+  "/root/src/pony/mlir/ShapeInferencePass.cpp" "pony/CMakeFiles/pony.dir/mlir/ShapeInferencePass.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/mlir/ShapeInferencePass.cpp.o.d"
+  "/root/src/pony/parser/AST.cpp" "pony/CMakeFiles/pony.dir/parser/AST.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/parser/AST.cpp.o.d"
+  "/root/src/pony/ponyc.cpp" "pony/CMakeFiles/pony.dir/ponyc.cpp.o" "gcc" "pony/CMakeFiles/pony.dir/ponyc.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/build/pony/CMakeFiles/pony.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for pony.
+# This may be replaced when dependencies are built.
